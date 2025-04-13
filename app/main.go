91b6cb8ad0357e5af66fd9f43b45dd2e373407ad
t3snake <t3snake@gmail.com>
2025-04-14 01:02:59 +0530

    implement hash-object command

--- app/main.go
+++ app/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
 	"fmt"
 	"os"
 	"strings"
 )
 
-// Usage: your_program.sh <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Fprintf(os.Stderr, "Logs from your program will appear here!\n")
 
 	if len(os.Args) < 2 {
@@ -20,8 +19,6 @@ func main() {
 
 	switch command := os.Args[1]; command {
 	case "init":
-		// Uncomment this block to pass the first stage!
-
 		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
 			if err := os.MkdirAll(dir, 0755); err != nil {
 				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
@@ -36,18 +33,36 @@ func main() {
 		fmt.Println("Initialized git directory")
 
 	case "cat-file":
-		content, err := uncompressZlib(os.Args[3])
+		content, err := CatFile(os.Args[3])
 		if err != nil {
 			fmt.Fprint(os.Stderr, err.Error())
 		}
 		fmt.Print(content)
+
+	case "hash-object":
+		var filename string
+		write_flag := os.Args[2] == "-w"
+
+		if write_flag {
+			filename = os.Args[3]
+		} else {
+			filename = os.Args[2]
+		}
+
+		hash, err := HashObject(filename, write_flag)
+		if err != nil {
+			fmt.Fprint(os.Stderr, err.Error())
+		}
+		fmt.Println(hash)
+
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
 	}
 }
 
-func uncompressZlib(hash string) (string, error) {
+// CatFile reads and returns contents of a blob object stored in .git given its hash.
+func CatFile(hash string) (string, error) {
 	file_name := ".git/objects/" + hash[0:2] + "/" + hash[2:]
 
 	file, err := os.ReadFile(file_name)
@@ -77,4 +92,48 @@ func uncompressZlib(hash string) (string, error) {
 		return "", fmt.Errorf("invalid blob: doesnt contain null byte")
 	}
 
+}
+
+// HashObject returns hash of given file as a blob. If write_flag is true, writes the blob into .git
+func HashObject(filename string, write_flag bool) (string, error) {
+	file_content, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	blob := generateBlobObject(string(file_content))
+
+	sha := sha1.Sum([]byte(blob))
+
+	if write_flag {
+		err = writeBlobObject(blob, sha)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	// convert fixed byte array to byte slice using [:] before converting to string
+	return string(sha[:]), nil
+}
+
+// generateBlobObject generates blob string and returns it
+func generateBlobObject(content string) string {
+	content_length := len(content)
+	return fmt.Sprintf("blob %d%s%s", content_length, string(rune(0)), content)
+}
+
+// writeBlobObject writes a blob into .git directory
+func writeBlobObject(blob_content string, sha_hash [20]byte) error {
+	// ".git/objects/" + hash[0:2] + "/" + hash[2:]
+	object_path := fmt.Sprintf(".git/objects/%s/%s", string(sha_hash[:2]), string(sha_hash[2:]))
+
+	// compress blob with zlib
+	var b bytes.Buffer
+	w := zlib.NewWriter(&b)
+	defer w.Close()
+
+	w.Write([]byte(blob_content))
+
+	err := os.WriteFile(object_path, b.Bytes(), 0644)
+	return err
 }
